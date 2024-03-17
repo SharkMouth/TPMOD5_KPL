bbe3cc068d8231035295d33a780914c6c621728a
SharkMouth <40568844+SharkMouth@users.noreply.github.com>
2024-03-18 01:23:08 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302223045/Program.cs
+++ tpmodul5_1302223045/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah : {data}");
+    }
+
+    
+}
+
+class Program
+{
+static void Main(string[] args)
+    {
+        DataGeneric<string> dataGeneric = new DataGeneric<string>("Y");
+        dataGeneric.PrintData();
+        DataGeneric<string> nim = new DataGeneric<string>("1302223045");
+        nim.PrintData();
+        Console.ReadLine();
+    }
+}
